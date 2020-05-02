a36e8783f5418b7ebc6b8cc5b5f4253707c2a5d5
Sthitaprajna Mishra <sthitaprajna360@gmail.com>
2020-05-02 13:03:47 +0530

    second

--- flaskblog.py
+++ flaskblog.py
@@ -4,14 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>HeY BOY!</h1>"
-
-@app.route("/about")
-def about():
-    return "<h1>About page</h1>"
-
-@app.route("/portfolio")
-def portfolio():
     return render_template('index.html')
 
 if __name__ == '__main__':
