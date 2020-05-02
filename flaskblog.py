0b4b687294816bf4b8b431c6a19532924f9bcc21
sthitaprajna-mishra <42604199+sthitaprajna-mishra@users.noreply.github.com>
2020-05-02 13:29:51 +0530

    Update flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+
+port = int(os.getenv("VCAP_APP_PORT"))
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = "0.0.0.0", port = port)
 
